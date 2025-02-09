a9570829af10fc010ec890ea6ddf6b9c8d4f86f3
Ariel-Tal <arital26102009@gmail.com>
2025-02-09 18:35:08 +0200

    algea fixed for main

--- src/main/java/frc/robot/subsystems/AlageaSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlageaSubsystem.java
@@ -7,11 +7,11 @@ package frc.robot.subsystems;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Rotations;
 
-import com.ctre.phoenix6.StatusSignal;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.units.Measure;
+
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
